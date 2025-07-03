f5fdfdd8410c3969d4ba51b13a8eab9e378f40f3
Luis J. Montes <luisjavm3@gmail.com>
2025-07-03 17:09:34 -0500

    make api-gateway discoverable

--- Photos-Server/src/main/java/com/appsdeveloperblog/ws/api/photos/security/WebSecurityConfig.java
+++ Photos-Server/src/main/java/com/appsdeveloperblog/ws/api/photos/security/WebSecurityConfig.java
@@ -27,14 +27,15 @@ public class WebSecurityConfig {
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter()); // delegate to custom
 
 		http.authorizeHttpRequests(
-				authz -> authz.requestMatchers(new AntPathRequestMatcher("/photos/**", "GET")).hasAuthority("ROLE_developer")
-				.anyRequest().authenticated())
-		.oauth2ResourceServer(
+				authz -> authz.requestMatchers(new AntPathRequestMatcher("/photos/**", "GET"))
+						.hasAuthority("ROLE_developer")
+						.anyRequest().authenticated())
+				.oauth2ResourceServer(
 						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
-		
+
 		http.sessionManagement((session) -> session
-		        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 		return http.build();
 
 	}
